a8b54a9fc6d0206a4a7a930be90d8c18cfbac30e
ra101 <agarwal.parth.101@gmail.com>
2024-08-22 23:46:40 +0530

    time reaper

--- grim_reapers/__init__.py
+++ grim_reapers/__init__.py
@@ -1,5 +1,7 @@
 from .signal_reaper import SignalReaper
+from .time_reaper import TimeReaper
 
 __all__ = [
     "SignalReaper",
+    "TimeReaper",
 ]
---
+++ grim_reapers/time_reaper.py
@@ -0,0 +1,45 @@
+import sys
+import threading
+from contextlib import ContextDecorator
+
+
+class TimeReaper(ContextDecorator):
+
+    stop_log = "Process stopped by timer after {time}s."
+
+    def __init__(
+        self, exit_callback,
+        stop_time=10, logger=sys.stdout.write,
+    ):
+        if stop_time <= 0:
+            raise AttributeError("`stop_time` should be a positive number.")
+
+        self.stop_time = stop_time
+        self.exit_callback = exit_callback
+        self.alarm_thread = None
+        self.logger = logger
+
+    def __enter__(self, *args, **kwargs):
+        self.set_alarm()
+
+    def __exit__(self, *args, **kwargs):
+        self.cancel_alarm()
+
+    def cancel_alarm(self):
+        if self.alarm_thread.is_alive():
+            self.alarm_thread.cancel()
+
+    def stop_process(self, *args, log=None):
+        self.cancel_alarm()
+
+        callback_return = self.exit_callback()
+        if self.logger:
+            self.logger(f"\n{log}\n")
+        return callback_return
+
+    def set_alarm(self):
+        self.alarm_thread = threading.Timer(
+            self.stop_time, self.stop_process,
+            kwargs={"log": self.stop_log.format(time=self.stop_time)},
+        )
+        self.alarm_thread.start()
